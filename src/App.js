34419da46c7b86aaa87582244f29f4a956ebedd1
Mohamad Faruk <mohamad.faruk@claromentis.com>
2024-10-26 18:57:05 +0100

    created web scraping front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Button } from '@mui/material'; 
+import TextField from '@mui/material/TextField';
+import Header from './components/Header'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [searchBar, setSearchBar] = React.useState("");
+    const [isExpanded, setIsExpanded] = React.useState(false);
+    const [displayResult, setDisplayResult] = React.useState(true); // Check to false for default
+
+    const toggleAdvancedSearch = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const setInput = (e) => {
+        setSearchBar(e.target.value);
+    };
+
+    const searchSponsors = () => {
+        // Implement your API calls here
+        console.log("Searching for sponsors:", searchBar);
+    };
+
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <div className="searchBar">
+                    <TextField
+                        type="search"
+                        id="search"
+                        name="search"
+                        label="Search sponsor..."
+                        value={searchBar}
+                        onChange={setInput}
+                        variant="outlined"
+                        sx={{
+                          '& .MuiInputBase-input': {
+                              color: 'white', // Text color
+                          },
+                          '& .MuiInputLabel-root': {
+                              color: 'white', // Label color
+                          },
+                          '& .MuiOutlinedInput-notchedOutline': {
+                            borderColor: 'white', // Border color for outlined variant
+                        },
+                        }}
+                    />
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        onClick={searchSponsors} 
+                        disabled={!searchBar}
+                        sx={{ backgroundColor: '#228B56'}}
+                    >
+                        Search
+                    </Button>
+                </div>
+
+                <div className="advancedSearch">
+                    <div>
+                        Advanced Search
+                        <Button onClick={toggleAdvancedSearch} className="arrowButton">
+                            {isExpanded ? '▲' : '▼'}
+                        </Button>
+                    </div>
+                    {isExpanded && (
+                        <>
+                            <div className="optionGroup">
+                                <label htmlFor="number">Number of attendees</label>
+                                <select name="number" id="number">
+                                    {/* Add options here */}
+                                </select>
+                            </div>
+
+                            <div className="optionGroup">
+                                <label htmlFor="format">In-person/online</label>
+                                <select name="format" id="format">
+                                    {/* Add options here */}
+                                </select>
+                            </div>
+
+                            <div className="optionGroup">
+                                <label htmlFor="location">Location</label>
+                                <select name="location" id="location">
+                                    {/* Add options here */}
+                                </select>
+                            </div>
+                        </>
+                    )}
+                </div>
+
+                {displayResult && (
+                    <>
+                        <div className="result">
+                            <div className='resultsHeader'>Results</div>
+                            <div className="cardGroup">
+                                {/* Example result cards */}
+                                <div className="resultCard">
+                                    <div>Sponsor Name 1</div>
+                                    <Button variant="contained" sx={{ backgroundColor: '#228B56'}}>More info</Button>
+                                </div>
+                                <div className="resultCard">
+                                    <div>Sponsor Name 2</div>
+                                    <Button variant="contained" sx={{ backgroundColor: '#228B56'}}>More info</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </main>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+    return <header><h1 className='searchBarTitle'>Search Sponsors</h1></header>;
+};
+
+export default Header;
